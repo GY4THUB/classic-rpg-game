58aaee90b84dc902d4590d854dc0d1b200238847
GoogleHub <abot01012001@gmail.com>
2025-07-25 13:47:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+<a href="Scenes/game.html" id="homepage">Start Game</a>
+
+<script>
+const homepage = document.getElementById("homepage");
+
+homepage.addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent default <a> link from navigating
+
+  function saveGame() {
+    const gameData = {
+      // Example variables (make sure these exist and are defined!)
+      mny,
+      dmg,
+      moneypersec,
+      priceofDMG,
+      incomePrice,
+      hp,
+      maxhp,
+      priceofHP,
+      userExp,
+      userMaxExp,
+      currentLvl,
+      wins,
+      losses,
+
+      normalLevel,
+      normalmaxhp,
+      normalDamage,
+      
+      eliteLevel,
+      elitemaxhp,
+      eliteDamage,
+      
+      soldierLevel,
+      soldiermaxhp,
+      soldierDamage,
+
+      veteranLevel,
+      veteranmaxhp,
+      veteranDamage,
+
+      masterLevel,
+      mastermaxhp,
+      masterDamage,
+
+      mastermindLevel,
+      mastermindmaxhp,
+      mastermindDamage,
+
+      thecauseLevel,
+      thecausemaxhp,
+      thecauseDamage,
+      
+      princeLevel,
+      princeHealth,
+      princemaxhp,
+      princeDamage,
+
+      queenLevel,
+      queenHealth,
+      queenmaxhp,
+      queenDamage,
+
+      kingLevel,
+      kingHealth,
+      kingmaxhp,
+      kingDamage,
+
+      memekingLevel,
+      memekingHealth,
+      memekingmaxhp,
+      memekingDamage,
+
+      gigachadLevel,
+      gigachadHealth,
+      gigachadmaxhp,
+      gigachadDamage,
+
+      brocodeLevel,
+      brocodeHealth,
+      brocodemaxhp,
+      brocodeDamage,
+
+      creatorLevel,
+      creatorHealth,
+      creatormaxhp,
+      creatorDamage
+    };
+
+    localStorage.setItem("idleGameSave", JSON.stringify(gameData));
+  }
+
+  // Save game first
+  saveGame();
+
+  // Then navigate
+  window.location.href = "Scenes/game.html";
+});
+</script>
